beeb302ef29c44c170c314859c9e01cd0475564b
madmax <anekdots1@yandex.ru>
2014-09-05 20:34:46 +0400

    Revert "Добавлен сервер"
    This reverts commit eaee345528f26bcbfc2d60dca0abc4ac641d8062.

--- CombatSituationServer/main.cpp
+++ CombatSituationServer/main.cpp
@@ -1,9 +1,4 @@
-#include <QCoreApplication>
-#include "TCPServer.h"
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    TCPServer server(2323, "battle_situation.txt");
-    return a.exec();
+int main() {
+    return 42;
 }
+
